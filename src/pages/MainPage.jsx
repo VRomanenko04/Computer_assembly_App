c1956fbd6b618991df3b3a57c60d7cf1a76a7e2b
VRomanenko04 <vovasan2004@gmail.com>
2023-06-02 04:03:41 +0300

    Сделал блок "партнёры"

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -17,6 +17,24 @@ export default function MainPage() {
                     <p className={classes.about_us_text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestias saepe. Pariatur natus itaque quisquam voluptatibus repellendus fuga alias maiores. Non quasi dolore amet earum, nulla architecto ab fugit iste.</p>
                 </article>
             </section>
+            <section className={classes.partners}>
+                <div className={classes.partners_head}>
+                    <img src="/imgs/Ellipse_partners.svg" alt='' className={classes.partners_logo}/>
+                    <h2 className={classes.partners_text}>Партнеры - топовые бренды</h2>
+                </div>
+                <div className={classes.grid_container}>
+                    <div className={classes.grid_comp}>
+                        <img src="/imgs/logo_msi.png" alt="" className={classes.partner}/>
+                        <img src="/imgs/logo_msi.png" alt="" className={classes.partner}/>
+                        <img src="/imgs/logo_msi.png" alt="" className={classes.partner}/>
+                        <img src="/imgs/logo_msi.png" alt="" className={classes.partner}/>
+                        <img src="/imgs/logo_msi.png" alt="" className={classes.partner}/>
+                        <img src="/imgs/logo_msi.png" alt="" className={classes.partner}/>
+                        <img src="/imgs/logo_msi.png" alt="" className={classes.partner}/>
+                        <img src="/imgs/logo_msi.png" alt="" className={classes.partner}/>
+                    </div>
+                </div>
+            </section>
         </main>
         </>
     )
