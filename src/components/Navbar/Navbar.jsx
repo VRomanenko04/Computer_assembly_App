7822ee2609206942bd13291ad3a97cc4056f1bba
VRomanenko04 <vovasan2004@gmail.com>
2023-06-16 01:06:24 +0300

    Убрал анимацию при перезагрузке

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import './navbar.css';
 import React, { useState } from 'react';
 import { Button } from '../UI/Button/Button';
 import { NavLink } from 'react-router-dom';
-import { easeInOut, motion } from 'framer-motion';
+import { AnimatePresence, easeInOut, motion } from 'framer-motion';
 import Media from 'react-media';
 
 export const Navbar = () => {
@@ -31,57 +31,57 @@ export const Navbar = () => {
                     <nav className='nav__body'>
                     <Media queries={{ small: '(max-width: 1040px)' }}>
                     {(matches) => (
-                        <motion.div
-                        className={`nav__list ${isOpen ? 'active__menu' : ''}`}
-                        initial={{ x: matches.small ? 300 : 0 }}
-                        animate={{ x: isOpen ? 0 : (matches.small ? 500 : 0)}}
-                        transition={{ ease: 'easeInOut', duration: 0.5 }}
-                        type={{easeInOut}}
-                    >
-                        <ul 
-                            className={`nav__list ${isOpen ? 'active__menu' : ''}`}
-                        >
-                            <motion.li 
-                                className='nav__li'
+                        <AnimatePresence initial={false}>
+                            <motion.div
+                                className={`nav__list ${isOpen ? 'active__menu' : ''}`}
                                 initial={{ x: matches.small ? 300 : 0 }}
-                                animate={{ x: isOpen ? 0 : (matches.small ? 1000 : 0)}}
-                                transition={{ ease: 'easeInOut', duration: 1 }}
+                                animate={{ x: isOpen ? 0 : (matches.small ? 500 : 0)}}
+                                transition={{ ease: 'easeInOut', duration: 0.5 }}
+                                type={{easeInOut}}
                             >
-                                <NavLink className='link' to='/'>Главная</NavLink>
-                            </motion.li>
-                            <motion.li 
-                                className='nav__li'
-                                initial={{ x: matches.small ? 300 : 0 }}
-                                animate={{ x: isOpen ? 0 : (matches.small ? 1000 : 0)}}
-                                transition={{ ease: 'easeInOut', duration: 1, delay: 0.1 }}
-                            >
-                                <NavLink className='link' to='/ready'>Готовые ПК</NavLink>
-                            </motion.li>
-                            <motion.li 
-                                className='nav__li'
-                                initial={{ x: matches.small ? 300 : 0 }}
-                                animate={{ x: isOpen ? 0 : (matches.small ? 1000 : 0)}}
-                                transition={{ ease: 'easeInOut', duration: 1, delay: 0.2 }}
-                            >
-                                <NavLink className='link' to='/individual'>Индивидуальная сборка</NavLink>
-                            </motion.li>
-                            <motion.li 
-                                className='nav__li'
-                                initial={{ x: matches.small ? 300 : 0 }}
-                                animate={{ x: isOpen ? 0 : (matches.small ? 1000 : 0)}}
-                                transition={{ ease: 'easeInOut', duration: 1, delay: 0.3 }}
-                            >
-                                <NavLink className='link' to='/contact'>Контакты</NavLink>
-                            </motion.li>
-                            <motion.li 
-                                className='nav__li' 
-                                initial={{ x: matches.small ? 300 : 0 }}
-                                animate={{ x: isOpen ? 0 : (matches.small ? 1000 : 0)}}
-                                transition={{ ease: 'easeInOut', duration: 1, delay: 0.4 }}>
-                                <Button link='/cabinet' text='Зайти в кабинет'/>
-                            </motion.li>
-                        </ul>
-                    </motion.div>
+                                <ul className={`nav__list ${isOpen ? 'active__menu' : ''}`}>
+                                    <motion.li 
+                                        className='nav__li'
+                                        initial={{ x: matches.small ? 300 : 0 }}
+                                        animate={{ x: isOpen ? 0 : (matches.small ? 1000 : 0)}}
+                                        transition={{ ease: 'easeInOut', duration: 1 }}
+                                    >
+                                        <NavLink className='link' to='/'>Главная</NavLink>
+                                    </motion.li>
+                                    <motion.li 
+                                        className='nav__li'
+                                        initial={{ x: matches.small ? 300 : 0 }}
+                                        animate={{ x: isOpen ? 0 : (matches.small ? 1000 : 0)}}
+                                        transition={{ ease: 'easeInOut', duration: 1, delay: 0.1 }}
+                                    >
+                                        <NavLink className='link' to='/ready'>Готовые ПК</NavLink>
+                                    </motion.li>
+                                    <motion.li 
+                                        className='nav__li'
+                                        initial={{ x: matches.small ? 300 : 0 }}
+                                        animate={{ x: isOpen ? 0 : (matches.small ? 1000 : 0)}}
+                                        transition={{ ease: 'easeInOut', duration: 1, delay: 0.2 }}
+                                    >
+                                        <NavLink className='link' to='/individual'>Индивидуальная сборка</NavLink>
+                                    </motion.li>
+                                    <motion.li 
+                                        className='nav__li'
+                                        initial={{ x: matches.small ? 300 : 0 }}
+                                        animate={{ x: isOpen ? 0 : (matches.small ? 1000 : 0)}}
+                                        transition={{ ease: 'easeInOut', duration: 1, delay: 0.3 }}
+                                    >
+                                        <NavLink className='link' to='/contact'>Контакты</NavLink>
+                                    </motion.li>
+                                    <motion.li 
+                                        className='nav__li' 
+                                        initial={{ x: matches.small ? 300 : 0 }}
+                                        animate={{ x: isOpen ? 0 : (matches.small ? 1000 : 0)}}
+                                        transition={{ ease: 'easeInOut', duration: 1, delay: 0.4 }}>
+                                        <Button link='/cabinet' text='Зайти в кабинет'/>
+                                    </motion.li>
+                                </ul>
+                            </motion.div>
+                        </AnimatePresence>
                     )}
                     </Media>
                     </nav>
