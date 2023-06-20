0691f20a684e8eb33c1a9c6675c4fde61a6a993e
VRomanenko04 <vovasan2004@gmail.com>
2023-06-20 21:31:31 +0300

    Сделал функционал Кабинета

--- src/pages/CabinetPage.jsx
+++ src/pages/CabinetPage.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+import classes from '../styles/cabinet.module.css';
 
 export const CabinetPage = () => {
+    const [isDataOpen, setIsDataOpen] = useState(true);
+    const [isDelivery, setIsDelivery] = useState(false);
+    const [isOrders, setIsOrders] = useState(false);
+    const [isSupport, setIsSupport] = useState(false);
+
+    const handleToggle = (stateToUpdate) => {
+        setIsDataOpen(stateToUpdate === 'isDataOpen' ? true : false);
+        setIsDelivery(stateToUpdate === 'isDelivery' ? true : false);
+        setIsOrders(stateToUpdate === 'isOrders' ? true : false);
+        setIsSupport(stateToUpdate === 'isSupport' ? true : false);
+    };
+
+    const buttons = [
+        { label: 'Мои данные', key: 'isDataOpen' },
+        { label: 'Доставка', key: 'isDelivery' },
+        { label: 'Мои заказы', key: 'isOrders' },
+        { label: 'Поддержка', key: 'isSupport' }
+    ];
+
     return (
-        <div>CabinetPage</div>
+        <main>
+            <div className={classes.container}>
+                <div className={classes.buttons}>
+                    {buttons.map(button => (
+                        <button
+                            key={button.key}
+                            className={`
+                                ${classes.btn} 
+                                ${button.key === 'isDataOpen' && isDataOpen ? classes.active : ''} 
+                                ${button.key === 'isDelivery' && isDelivery ? classes.active : ''} 
+                                ${button.key === 'isOrders' && isOrders ? classes.active : ''} 
+                                ${button.key === 'isSupport' && isSupport ? classes.active : ''}
+                            `}
+                            onClick={() => handleToggle(button.key)}
+                        >
+                            {button.label}
+                        </button>
+                    ))}
+                </div>
+                <div className={classes.changing__side}>
+                    {isDataOpen && (<p>Data</p>)}
+                    {isDelivery && (<p>Delivery</p>)}
+                    {isOrders && (<p>Orders</p>)}
+                    {isSupport && (<p>Support</p>)}
+                </div>
+            </div>
+        </main>
     )
 }
