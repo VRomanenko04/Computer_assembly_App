860e527d9a50d94a55f25c06404f0b14e6681fe7
VRomanenko04 <vovasan2004@gmail.com>
2023-06-23 00:01:49 +0300

    Доделал UserData, сохранение в localStorage

--- src/components/Blocks/UserDataBlock/UserData.jsx
+++ src/components/Blocks/UserDataBlock/UserData.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button} from '../../UI/Button/Button.jsx';
 import classes from './userdata.module.css';
 
@@ -8,8 +8,28 @@ export const UserData = () => {
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
 
+    useEffect(() => {
+        // Восстанавливаем сохраненные данные при загрузке компонента
+        const savedFirstName = localStorage.getItem('firstName');
+        const savedLastName = localStorage.getItem('lastName');
+        const savedPhone = localStorage.getItem('phone');
+        const savedEmail = localStorage.getItem('email');
+
+        if (savedFirstName) setFirstName(savedFirstName);
+        if (savedLastName) setLastName(savedLastName);
+        if (savedPhone) setPhone(savedPhone);
+        if (savedEmail) setEmail(savedEmail);
+    }, []);
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        // Сохраняем данные в localStorage
+        localStorage.setItem('firstName', firstName);
+        localStorage.setItem('lastName', lastName);
+        localStorage.setItem('phone', phone);
+        localStorage.setItem('email', email);
 
         // Сброс значений полей формы
         setFirstName('');
@@ -18,11 +38,13 @@ export const UserData = () => {
         setEmail('');
     };
 
+
     return (
-        <form onSubmit={handleSubmit}>
+        <form className={classes.form} onSubmit={handleSubmit}>
             <div className={classes.input__div}>
-                <label htmlFor="firstName">Имя:</label>
+                <p className={classes.text} htmlFor="firstName">Имя:</p>
                 <input
+                    className={classes.input}
                     type="text"
                     id="firstName"
                     value={firstName}
@@ -30,8 +52,9 @@ export const UserData = () => {
                 />
             </div>
             <div className={classes.input__div}>
-                <label htmlFor="lastName">Фамилия:</label>
+                <p className={classes.text} htmlFor="lastName">Фамилия:</p>
                 <input
+                    className={classes.input}
                     type="text"
                     id="lastName"
                     value={lastName}
@@ -39,24 +62,28 @@ export const UserData = () => {
                 />
             </div>
             <div className={classes.input__div}>
-                <label htmlFor="phone">Телефон:</label>
+                <p className={classes.text} htmlFor="phone">Телефон:</p>
                 <input
+                    className={classes.input}
                     type="text"
                     id="phone"
                     value={phone}
                     onChange={(e) => setPhone(e.target.value)}
+                    placeholder='050 000 0000'
                 />
             </div>
             <div className={classes.input__div}>
-                <label htmlFor="email">E-mail:</label>
+                <p className={classes.text} htmlFor="email">E-mail:</p>
                 <input
+                    className={classes.input}
                     type="email"
                     id="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
+                    placeholder='mail@gmail.com'
                 />
             </div>
-            <Button text='Сохранить'/>
+            <button className={classes.btn}>Сохранить</button>
         </form>
     )
 }
--- src/components/UI/BurgerBtn/BurgerBtn.jsx
+++ src/components/UI/BurgerBtn/BurgerBtn.jsx
@@ -6,7 +6,7 @@ export const BurgerBtn = ({handleMenu, isOpen}) => {
         <div className={cl.burger__btn} onClick={handleMenu}>
             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g id="Menu / Menu_Alt_01">
-                <path id="Vector" d="M12 17H19M5 12H19M5 7H19" stroke={isOpen ? '#ffffff' : '#7C6AFA'} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path id="Vector" d="M12 17H19M5 12H19M5 7H19" stroke={isOpen ? '#ffffff' : '#7C6AFA'} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </g>
             </svg>
         </div>
