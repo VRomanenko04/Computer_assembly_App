b0f3186b6ac046456ec4a3782e237ebf8f8da524
VRomanenko04 <vovasan2004@gmail.com>
2023-06-23 19:01:33 +0300

    Сделал преобретение товара в localStorage

--- src/components/Computer/Computer.jsx
+++ src/components/Computer/Computer.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 import cl from './computer.module.css';
-import {Button} from '../UI/Button/Button.jsx';
 
 export const Computer = (props) => {
+
+    const handleBuyClick = () => {
+        Swal.fire({
+            title: 'Введите ваш номер телефона',
+            input: 'text',
+            showCancelButton: true,
+            confirmButtonText: 'Купить',
+            cancelButtonText: 'Отмена',
+            inputValidator: (value) => {
+                if (!value) {
+                    return 'Вы должны ввести номер телефона';
+                }
+            },
+            preConfirm: (phone) => {
+                const purchasedPCs = JSON.parse(localStorage.getItem('purchasedPCs')) || [];
+                // Проверка наличия уже существующей покупки с таким же названием
+                const existingPurchase = purchasedPCs.find((pc) => pc.name === props.head);
+                if (existingPurchase) {
+                    Swal.fire({
+                        title: 'Ошибка',
+                        text: 'Вы уже приобрели этот товар',
+                        icon: 'error'
+                    });
+                    return false; // Отменить сохранение покупки
+                }
+                // Сохраните название купленного ПК и номер телефона в локальное хранилище
+                const purchasedPC = {
+                    name: props.head,
+                    phone: phone
+                };
+                purchasedPCs.push(purchasedPC);
+                localStorage.setItem('purchasedPCs', JSON.stringify(purchasedPCs));
+            }
+        }).then((result) => {
+            if (result.value) {
+                Swal.fire({
+                    title: 'Ваш заказ подтвержден',
+                    text: 'Ожидайте звонка',
+                    icon: 'success'
+                });
+            }
+        });
+    };
+
     return (
         <div className={cl.block__container}>
             <img className={cl.photo} src={props.photo} alt="" />
@@ -14,7 +58,7 @@ export const Computer = (props) => {
                 <li className={cl.list__item}>{props.memory}</li>
             </ul>
             <p className={cl.price}>{props.price}</p>
-            <Button text='КУПИТЬ' additionalClass={cl.button}/>
+            <button className={cl.btn} onClick={handleBuyClick}>КУПИТЬ</button>
         </div>
     )
 }
