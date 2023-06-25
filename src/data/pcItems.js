ef055a021c4df051cafd981bfbc7a1700fa99158
VRomanenko04 <vovasan2004@gmail.com>
2023-06-26 02:49:05 +0300

    Аккордион, выбор комплектующих, подсчёт цены

--- src/data/pcItems.js
+++ src/data/pcItems.js
@@ -1,282 +1,282 @@
-const PcItems = [
+export const PcItems = [
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E GeForce GTX1630 4GB DDR6 (GTX 1630 AERO ITX 4G OC)',
-        price: '6300'
+        price: 6300
     },
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E Radeon RX 6500 XT 4GB DDR6 (RX 6500 XT MECH 2X 4G OC)',
-        price: '7600'
+        price: 7600
     },
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E GeForce GTX1660 Super 6GB DDR6 (GTX 1660 SUPER VENTUS XS)',
-        price: '11799'
+        price: 11799
     },
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E GeForce GT1030 2GB DDR4 (GT 1030 2GHD4 LP OC)',
-        price: '3679'
+        price: 3679
     },
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E GeForce RTX3050 LHR 8GB DDR6 (RTX 3050 VENTUS 2X 8G OC)',
-        price: '10260'
+        price: 10260
     },
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E Radeon RX 6650 XT 8GB DDR6 (RX 6650 XT Mech 2X 8G OC)',
-        price: '14199'
+        price: 14199
     },
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E GeForce RTX3060 LHR 12GB DDR6 (RTX 3060 VENTUS 2X 12G OC)',
-        price: '15399'
+        price: 15399
     },
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E GeForce RTX4060 Ti 8GB DDR6 (RTX 4060 Ti GAMING X 8G)',
-        price: '20499'
+        price: 20499
     },
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E GeForce RTX4060 Ti 8GB DDR6 (RTX 4060 Ti GAMING X TRIO 8G)',
-        price: '21999'
+        price: 21999
     },
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E GeForce RTX4070 12GB DDR6X (RTX 4070 VENTUS 2X 12G OC)',
-        price: '28299'
+        price: 28299
     },
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E Radeon RX 7900 XT 20GB DDR6 (RX 7900 XT Gaming Trio Classic 20G)',
-        price: '41599'
+        price: 41599
     },
     {
         type: 'videocard',
         name: 'Відеокарта MSI PCI-E GeForce RTX4080 16GB DDR6X (RTX 4080 16GB GAMING X TRIO WHITE)',
-        price: '61999'
+        price: 61999
     },
     {
         type: 'proc',
         name: 'Процесор Intel Core i3-12100F s-1700 3.3GHz/12MB Tray',
-        price: '4279'
+        price: 4279
     },
     {
         type: 'proc',
         name: 'Процесор Intel Core i3-12100 s-1700 3.3GHz/12MB BOX',
-        price: '5259'
+        price: 5259
     },
     {
         type: 'proc',
         name: 'Процесор Intel Core i5-9400F s-1151 2.9GHz/9MB BOX',
-        price: '6255'
+        price: 6255
     },
     {
         type: 'proc',
         name: 'Процесор Intel Core i5-10500 s-1200 3.1GHz/12MB BOX',
-        price: '7319'
+        price: 7319
     },
     {
         type: 'proc',
         name: 'Процесор Intel Core i7-10700 s-1200 2.9GHz/16MB BOX',
-        price: '9799'
+        price: 9799
     },
     {
         type: 'proc',
         name: 'Процесор Intel Core i7-12700F s-1700 4.9GHz/25MB BOX',
-        price: '13199'
+        price: 13199
     },
     {
         type: 'proc',
         name: 'Процесор Intel Core i9-11900K s-1200 3.5GHz/16MB BOX',
-        price: '14400'
+        price: 14400
     },
     {
         type: 'proc',
         name: 'Процесор Intel Core i9-12900F s-1700 5.1GHz/30MB BOX',
-        price: '21099'
+        price: 21099
     },
     {
         type: 'proc',
         name: 'Процесор Intel Core i7-13700KF s-1700 3.4GHz/30MB BOX',
-        price: '17399'
+        price: 17399
     },
     {
         type: 'motherboard',
         name: 'Материнська плата MSI B560M Pro-E s-1200 B560',
-        price: '3299'
+        price: 3299
     },
     {
         type: 'motherboard',
         name: 'Материнська плата MSI MPG B550 Gaming PLUS s-AM4 B550',
-        price: '6099'
+        price: 6099
     },
     {
         type: 'motherboard',
         name: 'Материнська плата MSI Pro Z790-A WIFI s-1700 Z790',
-        price: '11399'
+        price: 11399
     },
     {
         type: 'motherboard',
         name: 'Материнська плата MSI MPG X670E Carbon WiFi s-AM5 X670',
-        price: '21085'
+        price: 21085
     },
     {
         type: 'motherboard',
         name: 'Материнська плата MSI B550-A Pro s-AM4 B550',
-        price: '5545'
+        price: 5545
     },
     {
         type: 'motherboard',
         name: 'Материнська плата MSI MPG B650 Edge WIFI s-AM5 B650',
-        price: '12799'
+        price: 12799
     },
     {
         type: 'motherboard',
         name: 'Материнська плата MSI B450 Gaming Plus Max s-AM4 B450',
-        price: '4199'
+        price: 4199
     },
     {
         type: 'motherboard',
         name: 'Материнська плата MSI MPG X570S Carbon Max WIFI s-AM4 X570',
-        price: '14000'
+        price: 14000
     },
     {
         type: 'ram',
         name: 'Kingston Fury DDR4 16GB 2x8GB 3200MHz Beast Black',
-        price: '1719'
+        price: 1719
     },
     {
         type: 'ram',
         name: 'Kingston Fury DDR4 32GB 2x16GB 3200MHz Beast Black',
-        price: '3321'
+        price: 3321
     },
     {
         type: 'ram',
         name: 'Kingston Fury DDR4 64GB 4x16GB 3200MHz Beast Black',
-        price: '6630'
+        price: 6630
     },
     {
         type: 'ram',
         name: 'Kingston Fury DDR4 8GB 3200MHz Beast Black',
-        price: '890'
+        price: 890
     },
     {
         type: 'cooling',
         name: 'DeepCool AK400',
-        price: '1369'
+        price: 1369
     },
     {
         type: 'cooling',
         name: 'ID-Cooling SE-903-SD',
-        price: '499'
+        price: 499
     },
     {
         type: 'cooling',
         name: 'be quiet! Dark Rock 4',
-        price: '3119'
+        price: 3119
     },
     {
         type: 'cooling',
         name: 'DeepCool AS500 PLUS White',
-        price: '3119'
+        price: 3119
     },
     {
         type: 'cooling',
         name: 'DeepCool AG400 LED',
-        price: '1180'
+        price: 1180
     },
     {
         type: 'memory',
         name: 'SSD-накопичувач M.2 1TB Samsung 980',
-        price: '2489'
+        price: 2489
     },
     {
         type: 'memory',
         name: 'SSD-накопичувач 2.5" SATA 500GB Samsung 870 EVO',
-        price: '1629'
+        price: 1629
     },
     {
         type: 'memory',
         name: 'SSD-накопичувач M.2 2TB Samsung 980 PRO',
-        price: '5799'
+        price: 5799
     },
     {
         type: 'memory',
         name: 'SSD-накопичувач 2.5" SATA 256GB Kingston KC600',
-        price: '1079'
+        price: 1079
     },
     {
         type: 'memory',
         name: 'Жорсткий диск 3.5" SATA 1TB WD Blue',
-        price: '1435'
+        price: 1435
     },
     {
         type: 'memory',
         name: 'Жорсткий диск 3.5" SATA 4TB Seagate SkyHawk Surveillance',
-        price: '3000'
+        price: 3000
     },
     {
         type: 'memory',
         name: 'Жорсткий диск 3.5" SATA 2TB Seagate Barracuda',
-        price: '1890'
+        price: 1890
     },
     {
         type: 'frame',
         name: 'Корпус 1stPlayer V3-A-4G6 Black',
-        price: '1499'
+        price: 1499
     },
     {
         type: 'frame',
         name: 'Корпус MSI MAG Forge 100M',
-        price: '2899'
+        price: 2899
     },
     {
         type: 'frame',
         name: 'Корпус MSI MAG Vampiric 300R',
-        price: '5500'
+        price: 5500
     },
     {
         type: 'frame',
         name: 'Корпус MSI MPG Quietude 100S',
-        price: '6999'
+        price: 6999
     },
     {
         type: 'frame',
         name: 'Корпус Gamemax White Diamond COC',
-        price: '2350'
+        price: 2350
     },
     {
         type: 'frame',
         name: 'Корпус Lian Li O11 Dynamic XL б/БЖ White',
-        price: '11870'
+        price: 11870
     },
     {
         type: 'frame',
         name: 'Корпус Razer Tomahawk',
-        price: '9999'
+        price: 9999
     },
     {
         type: 'power',
         name: 'Блок живлення 600W AeroCool VX 600 Plus',
-        price: '1399'
+        price: 1399
     },
     {
         type: 'power',
         name: 'Блок живлення 750W AeroCool Mirage Gold 750',
-        price: '3899'
+        price: 3899
     },
     {
         type: 'power',
         name: 'Блок живлення 850W Gigabyte',
-        price: '4320'
+        price: 4320
     },
     {
         type: 'power',
         name: 'Блок живлення 1000W Corsair RM1000e',
-        price: '7899'
+        price: 7899
     },
 ]
--- src/pages/IndividualPcPage.jsx
+++ src/pages/IndividualPcPage.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from '../styles/individualPC.module.css';
-import {PcItems} from '../data/pcItems';
+import { PcItems } from '../data/pcItems.js';
+
+const pcComponents = PcItems;
 
 export const IndividualPcPage = () => {
-    const TotalPrice = 799;
-    const pcComponents = PcItems;
+    const [selectedType, setSelectedType] = useState(null);
+    const [selectedComponents, setSelectedComponents] = useState({});
+    const [totalPrice, setTotalPrice] = useState(799);
+
+    const handleAccordionClick = (type) => {
+        setSelectedType((prevSelectedType) => (prevSelectedType === type ? null : type));
+    };
+
+    const handleComponentClick = (type, component) => {
+        setSelectedComponents((prevSelectedComponents) => {
+            const updatedComponents = { ...prevSelectedComponents };
+    
+            if (updatedComponents[type] === component) {
+                delete updatedComponents[type];
+            } else {
+                updatedComponents[type] = component;
+            }
+    
+            let newTotalPrice = 799; // Установка начальной суммы
+    
+            // Пересчет суммы на основе выбранных компонентов
+            Object.values(updatedComponents).forEach((selectedComponent) => {
+                newTotalPrice += Number(selectedComponent.price);
+            });
+    
+            setTotalPrice(newTotalPrice);
+            return updatedComponents;
+        });
+    };
+
+    const filteredComponents = selectedType
+    ? pcComponents.filter((component) => component.type === selectedType)
+    : pcComponents;
 
     return (
         <main className={classes.main}>
@@ -12,9 +45,36 @@ export const IndividualPcPage = () => {
                 <div className={classes.service}>
                     <p>Услуга сборки и тестирования ПК - <strong>799 грн</strong></p>
                 </div>
+                <div className={classes.accordionContainer}>
+                    {['videocard', 'proc', 'motherboard', 'ram', 'cooling', 'memory', 'frame', 'power'].map((type) => (
+                        <div key={type} className={classes.accordion}>
+                            <div
+                                className={selectedType === type ? classes.accordionButtonActive : classes.accordionButton}
+                                onClick={() => handleAccordionClick(type)}
+                            >
+                                {type}
+                            </div>
+                            {selectedType === type && (
+                                <ul className={classes.componentList}>
+                                    {filteredComponents.map((component) => (
+                                        <li
+                                            key={component.name}
+                                            className={`${classes.componentItem} ${
+                                                selectedComponents[type] === component ? classes.componentItemActive : ''
+                                            }`}
+                                            onClick={() => handleComponentClick(type, component)}
+                                        >
+                                            <p>{component.name} - <strong>{component.price} грн</strong></p>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                    ))}
+                </div>
             </div>
             <div className={classes.total__order}>
-                <p className={classes.total__price}>Конечная сумма: <strong>{TotalPrice} грн</strong></p>
+                <p className={classes.total__price}>Конечная сумма: <strong>{totalPrice} грн</strong></p>
                 <button className={classes.btn}>Заказать</button>
             </div>
         </main>
