afdec12efa78b42d1b8d695d6b82e73548f1e770
VRomanenko04 <vovasan2004@gmail.com>
2023-06-26 00:06:50 +0300

    Сделал базу под страницу "Индивидуальная сборка"

--- src/pages/IndividualPcPage.jsx
+++ src/pages/IndividualPcPage.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import classes from '../styles/individualPC.module.css';
 
 export const IndividualPcPage = () => {
+    const TotalPrice = 799;
     return (
-        <div>IndividualPcPage</div>
+        <main className={classes.main}>
+            <div className={classes.container}>
+                <div className={classes.service}>
+                    <p>Услуга сборки и тестирования ПК - <strong>799 грн</strong></p>
+                </div>
+            </div>
+            <div className={classes.total__order}>
+                <p className={classes.total__price}>Конечная сумма: <strong>{TotalPrice} грн</strong></p>
+                <button className={classes.btn}>Заказать</button>
+            </div>
+        </main>
     )
 }
