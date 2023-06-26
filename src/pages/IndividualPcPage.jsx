a557cecf62afc20afc6394dd80ce9df2b7254d13
VRomanenko04 <vovasan2004@gmail.com>
2023-06-27 00:24:52 +0300

    Доделал аккордионы, условный рендер на кнопку

--- src/pages/IndividualPcPage.jsx
+++ src/pages/IndividualPcPage.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from '../styles/individualPC.module.css';
 import { PcItems } from '../data/pcItems.js';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const pcComponents = PcItems;
 
@@ -8,6 +9,7 @@ export const IndividualPcPage = () => {
     const [selectedType, setSelectedType] = useState(null);
     const [selectedComponents, setSelectedComponents] = useState({});
     const [totalPrice, setTotalPrice] = useState(799);
+    const [isOrderEnabled, setIsOrderEnabled] = useState(false);
 
     const handleAccordionClick = (type) => {
         setSelectedType((prevSelectedType) => (prevSelectedType === type ? null : type));
@@ -35,6 +37,12 @@ export const IndividualPcPage = () => {
         });
     };
 
+    useEffect(() => {
+        // Проверка наличия выбранного компонента в каждом аккордеоне
+        const isOrderEnabled = Object.keys(selectedComponents).length === 8;
+        setIsOrderEnabled(isOrderEnabled);
+    }, [selectedComponents]);
+
     const filteredComponents = selectedType
     ? pcComponents.filter((component) => component.type === selectedType)
     : pcComponents;
@@ -48,34 +56,47 @@ export const IndividualPcPage = () => {
                 <div className={classes.accordionContainer}>
                     {['videocard', 'proc', 'motherboard', 'ram', 'cooling', 'memory', 'frame', 'power'].map((type) => (
                         <div key={type} className={classes.accordion}>
-                            <div
+                            <motion.div
                                 className={selectedType === type ? classes.accordionButtonActive : classes.accordionButton}
                                 onClick={() => handleAccordionClick(type)}
                             >
                                 {type}
-                            </div>
-                            {selectedType === type && (
-                                <ul className={classes.componentList}>
-                                    {filteredComponents.map((component) => (
-                                        <li
-                                            key={component.name}
-                                            className={`${classes.componentItem} ${
-                                                selectedComponents[type] === component ? classes.componentItemActive : ''
-                                            }`}
-                                            onClick={() => handleComponentClick(type, component)}
-                                        >
-                                            <p>{component.name} - <strong>{component.price} грн</strong></p>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
+                            </motion.div>
+                            <AnimatePresence>
+                                {selectedType === type && (
+                                    <motion.ul 
+                                        className={classes.componentList}
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
+                                        exit={{ opacity: 0 }}
+                                    >
+                                        {filteredComponents.map((component) => (
+                                            <motion.li
+                                                key={component.name}
+                                                className={`${classes.componentItem} ${
+                                                    selectedComponents[type] === component ? classes.componentItemActive : ''
+                                                }`}
+                                                onClick={() => handleComponentClick(type, component)}
+                                            >
+                                                <p>{component.name} - <strong>{component.price} грн</strong></p>
+                                            </motion.li>
+                                        ))}
+                                    </motion.ul>
+                                )}
+                            </AnimatePresence>
                         </div>
                     ))}
                 </div>
             </div>
             <div className={classes.total__order}>
                 <p className={classes.total__price}>Конечная сумма: <strong>{totalPrice} грн</strong></p>
-                <button className={classes.btn}>Заказать</button>
+                {isOrderEnabled ? 
+                ( <button className={classes.btn}>Заказать</button> ) : 
+                (
+                    <div className={classes.message}>
+                        <p>Выберите все комплектующие для заказа</p>
+                    </div>
+                )}
             </div>
         </main>
     )
