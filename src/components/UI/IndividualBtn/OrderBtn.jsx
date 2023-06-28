03773125ac538397b875551dd5473dc906214206
VRomanenko04 <vovasan2004@gmail.com>
2023-06-28 23:32:09 +0300

    Доделал процесс покупки и отображение инд. заказа

--- src/components/UI/IndividualBtn/OrderBtn.jsx
+++ src/components/UI/IndividualBtn/OrderBtn.jsx
@@ -4,13 +4,13 @@ import classes from './orderbtn.module.css';
 export const OrderBtn = (props) => {
     const {
         isOrderEnabled,
-        handleOrderClick
+        handleModal
     } = props;
 
     return (
         <>
             {isOrderEnabled ? 
-            ( <button className={classes.btn} onClick={handleOrderClick}>Заказать</button> ) 
+            ( <button className={classes.btn} onClick={handleModal}>Заказать</button> ) 
             : (
                 <div className={classes.message}>
                     <p>Выберите все комплектующие для заказа</p>
