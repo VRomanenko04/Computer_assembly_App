3ca056372841369cf21a29ca6dfc7a7899415350
VRomanenko04 <vovasan2004@gmail.com>
2023-07-01 03:36:23 +0300

    Добавил анимацию на main page

--- src/components/Blocks/AboutUsBlock/AboutUs.jsx
+++ src/components/Blocks/AboutUsBlock/AboutUs.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import classes from './about.module.css';
 import about from '../../../assets/about__img.png';
+import { motion } from 'framer-motion';
+
+const sectionAnimate = {
+    hidden: {
+        y: -50,
+        opacity: 0,
+    },
+    visible: custom => ({
+        y: 0,
+        opacity: 1,
+        transition: {delay: custom * 0.2}
+    })
+}
 
 export const AboutUs = () => {
     return (
-        <section className={classes.about__us}>
+        <motion.section
+            initial='hidden'
+            viewport={{ amount: 0.2, once: true }}
+            whileInView='visible'
+            className={classes.about__us}
+        >
             <img className={classes.about__img} src={about} alt=''/>
             <article>
-                <h2 className={classes.about_us_head}>Чем мы занимаемся?</h2>
-                <p className={classes.about_us_text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestias saepe. Pariatur natus itaque quisquam voluptatibus repellendus fuga alias maiores. Non quasi dolore amet earum, nulla architecto ab fugit iste.</p>
+                <motion.h2 custom={1} variants={sectionAnimate} className={classes.about_us_head}>Чем мы занимаемся?</motion.h2>
+                <motion.p custom={2} variants={sectionAnimate} className={classes.about_us_text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestias saepe. Pariatur natus itaque quisquam voluptatibus repellendus fuga alias maiores. Non quasi dolore amet earum, nulla architecto ab fugit iste.</motion.p>
             </article>
-        </section>
+        </motion.section>
     )
 }
--- src/components/Blocks/PartnersBlock/Partners.jsx
+++ src/components/Blocks/PartnersBlock/Partners.jsx
@@ -1,28 +1,68 @@
 import React from 'react';
 import classes from './partners.module.css';
+import { motion } from 'framer-motion';
 
 import ellipse from '../../../assets/Ellipse_partners.svg';
 import logoMSI from '../../../assets/logo_msi.png';
 
+const sectionAnimate = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: custom => ({
+        opacity: 1,
+        transition: {delay: custom * 0.2}
+    })
+}
+
 export const Partners = () => {
     return (
-        <section className={classes.partners}>
+        <motion.section 
+            initial='hidden'
+            viewport={{ once: true }}
+            whileInView='visible'
+            className={classes.partners}
+        >
             <div className={classes.partners_head}>
-                <img src={ellipse} alt='' className={classes.partners_logo}/>
-                <h2 className={classes.partners_text}>Партнеры - топовые бренды</h2>
+                <motion.img custom={1} variants={sectionAnimate} src={ellipse} alt='' className={classes.partners_logo}/>
+                <motion.h2 custom={1} variants={sectionAnimate} className={classes.partners_text}>Партнеры - топовые бренды</motion.h2>
             </div>
             <div className={classes.grid_container}>
                 <div className={classes.grid_comp}>
-                    <img src={logoMSI} alt="" className={classes.partner__img}/>
-                    <img src={logoMSI} alt="" className={classes.partner__img}/>
-                    <img src={logoMSI} alt="" className={classes.partner__img}/>
-                    <img src={logoMSI} alt="" className={classes.partner__img}/>
-                    <img src={logoMSI} alt="" className={classes.partner__img}/>
-                    <img src={logoMSI} alt="" className={classes.partner__img}/>
-                    <img src={logoMSI} alt="" className={classes.partner__img}/>
-                    <img src={logoMSI} alt="" className={classes.partner__img}/>
+                    <motion.img 
+                        custom={1} variants={sectionAnimate} 
+                        src={logoMSI} alt="" className={classes.partner__img}
+                    />
+                    <motion.img 
+                        custom={1.5} variants={sectionAnimate} 
+                        src={logoMSI} alt="" className={classes.partner__img}
+                    />
+                    <motion.img 
+                        custom={2} variants={sectionAnimate} 
+                        src={logoMSI} alt="" className={classes.partner__img}
+                    />
+                    <motion.img 
+                        custom={2.5} variants={sectionAnimate} 
+                        src={logoMSI} alt="" className={classes.partner__img}
+                    />
+                    <motion.img 
+                        custom={3} variants={sectionAnimate} 
+                        src={logoMSI} alt="" className={classes.partner__img}
+                    />
+                    <motion.img 
+                        custom={3.5} variants={sectionAnimate} 
+                        src={logoMSI} alt="" className={classes.partner__img}
+                    />
+                    <motion.img 
+                        custom={4} variants={sectionAnimate} 
+                        src={logoMSI} alt="" className={classes.partner__img}
+                    />
+                    <motion.img 
+                        custom={4.5} variants={sectionAnimate} 
+                        src={logoMSI} alt="" className={classes.partner__img}
+                    />
                 </div>
             </div>
-        </section>
+        </motion.section>
     )
 }
